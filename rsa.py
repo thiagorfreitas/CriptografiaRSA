29ec36b86ee0ddeb5fed7d7c97dcecea0f3b7b02
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2023-11-27 21:23:59 -0300

    V1.0

--- rsa.py
+++ rsa.py
@@ -1,20 +1,23 @@
+print("Decifre uma criptografia RSA")
 x = input("Digite os números separados por espaço: ").split()
-y = int(input("Digite o expoente: "))
-z = int(input("Digite o divisor: "))
+y = int(input("Digite o valor de B (beta): "))
+z = int(input("Digite o valor de N: "))
+# Criando um dicionário de acordo com a tabela utilizada de conversão de números para caracteres
 dic_letras = {i: chr(i - 10 + ord('A')) for i in range(10, 36)}#{chr(i): i - ord('A') + 10 for i in range(ord('A'), ord('Z') + 1)}
 dic_letras[36] = ' '
 
-# Use list comprehension para converter elementos para inteiros e calcular resultados
+# List comprehension para converter elementos para inteiros e calcular resultados
 results = [str(int(num) ** y % z) for num in x]
 
-# Combine os resultados em uma única string
+# Combinando os resultados em uma única string
 res = ' '.join(results)
 s = ''.join(results)
-# Separe os resultados em pares de dois caracteres
+# Separando os resultados em pares de dois caracteres
 res_splitted = ' '.join([s[i:i+2] for i in range(0, len(s), 2)])
 letra = []
 print("Resultados:", res)
 print("Resultados Agrupados:", res_splitted)
+# Decodificando a palavra
 for i in range(0, len(res_splitted.split())):
     key = int(res_splitted.split()[i])
     letra.append(dic_letras.get(key, 'Chave nao encontrada'))
